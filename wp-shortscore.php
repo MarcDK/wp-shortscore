dd60c95207318c9ecc1857e9cf4c0a9a35ad3cf2
Marc Tönsing <marc@marc.tv>
2018-09-29 10:34:38 +0200

    v3.5

--- wp-shortscore.php
+++ wp-shortscore.php
@@ -15,7 +15,7 @@ License URI: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Class WpShortscore
  */
 class WpShortscore {
-	private $version = '3.4';
+	private $version = '3.5';
 
 	/**
 	 * WpShortscore constructor.
